7e6dbcfe3bb4668503d0c707991a5bb15370451d
Rafael da Silva <rafaeltimoreirass@gmail.com>
2026-03-16 00:04:25 -0300

    fix: move types to dependencies for render build support

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -6,12 +6,17 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const knexConfig: Knex.Config = {
   client: 'pg',
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host: appConfig.database.host,
     port: appConfig.database.port,
     database: appConfig.database.name,
     user: appConfig.database.user,
     password: appConfig.database.password,
+    ssl: isProd ? { rejectUnauthorized: false } : false,
+  },
+  pool: {
+    min: 2,
+    max: 10
   },
   migrations: {
     directory: isProd 
